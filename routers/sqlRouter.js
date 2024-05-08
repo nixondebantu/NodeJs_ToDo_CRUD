4378c1577ea9ef01d2e12214bc7a9d57142a917e
Nixon Deb Antu <nixondebantu@gmail.com>
2024-05-08 18:56:51 +0600

    Create User Registration API

--- routers/sqlRouter.js
+++ routers/sqlRouter.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const createAdmin = require("../controllers/v2/createAdmin");
+const register = require("../controllers/v2/register");
 
 const sqlRouter = express.Router();
 
 sqlRouter.post("/createadmin",createAdmin);
-
+sqlRouter.post("/register",register);
 
 module.exports = sqlRouter;
